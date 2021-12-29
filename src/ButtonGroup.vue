0fea4462f731c03f3fdff8a7214bf0a20b0cd1bc
hushuo <1253740807@qq.com>
2021-12-29 20:11:15 +0800

    修改BUG  button-group让其第一个元素margin不用 -1px

--- src/ButtonGroup.vue
+++ src/ButtonGroup.vue
@@ -7,15 +7,16 @@
 <script>
 export default {
   name: "ButtonGroup",
-  mounted(){
-
-      for(let node of this.$el.children){
-          let name = node.nodeName.toLowerCase()
-          if(name !== 'button'){
-              console.warn(`g-button-group的子元素应该全是g-button 但是你写的是${name}`);
-          }
+  mounted() {
+    for (let node of this.$el.children) {
+      let name = node.nodeName.toLowerCase();
+      if (name !== "button") {
+        console.warn(
+          `g-button-group的子元素应该全是g-button 但是你写的是${name}`
+        );
       }
-  }
+    }
+  },
 };
 </script>
 
@@ -25,12 +26,12 @@ export default {
   vertical-align: middle; //上下不对齐可以添加此属性
   > .g-button {
     border-radius: 0;
-    margin-left: -1px;
-    //   &:first-child{
-    //问题： 在最左边时会有1px位移  TODO
-    //   &:not(:first-child){
-    //     border-left: none;边框颜色会缺失
-    //     }
+    &:not(:first-child) {
+      margin-left: -1px;
+      //问题： 在最左边时会有1px位移  TODO
+      //   &:not(:first-child){
+      //     border-left: none;边框颜色会缺失
+    }
     &:first-child {
       border-top-left-radius: var(--border-radius);
       border-bottom-left-radius: var(--border-radius);
