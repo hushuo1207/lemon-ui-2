ceec77f5732b473159d7715e1fa53dc3c09810a2
hushuo <1253740807@qq.com>
2022-01-02 01:33:51 +0800

    添加部分测试用例

---
+++ test/tabs-item.test.js
@@ -0,0 +1,46 @@
+const expect = chai.expect;
+import Vue from 'vue'
+import Tabs from '../src/Tabs'
+import TabsHead from '../src/TabsHead'
+import TabsBody from '../src/TabsBody'
+import TabsItem from '../src/TabsItem'
+import TabsPane from '../src/TabsPane'
+
+Vue.component('g-tabs', Tabs)
+Vue.component('g-tabs-head', TabsHead)
+Vue.component('g-tabs-body', TabsBody)
+Vue.component('g-tabs-item', TabsItem)
+Vue.component('g-tabs-pane', TabsPane)
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('TabsItem', () => {
+
+  it('存在.', () => {
+    expect(TabsItem).to.exist
+  })
+
+  it('接受 name 属性', () => {
+    const Constructor = Vue.extend(TabsItem)
+    const vm = new Constructor({
+      propsData: {
+        name: 'xxx',
+      }
+    }).$mount()
+    expect(vm.$el.getAttribute('data-name')).to.eq('xxx')
+  })
+  it('接受 disabled 属性', () => {
+    const Constructor = Vue.extend(TabsItem)
+    const vm = new Constructor({
+      propsData: {
+        disabled: true,
+      }
+    }).$mount()
+    expect(vm.$el.classList.contains('disabled')).to.be.true
+    const callback = sinon.fake();
+    vm.$on('click', callback)
+    vm.$el.click()
+    expect(callback).to.have.not.been.called
+  })
+}) 
