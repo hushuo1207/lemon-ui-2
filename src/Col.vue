f8a80fe0fbb69f37afa1f19ce2dc0d968384459d
hushuo <1253740807@qq.com>
2022-01-01 12:20:14 +0800

    Row and Col基本完成

--- src/Col.vue
+++ src/Col.vue
@@ -1,6 +1,11 @@
 <template>
-    <div class="col" :class="[`col-${span}`]">
-        <slot></slot>
+    <div class="col" 
+        :class="[span && `col-${span}`, offset && `offset-${offset}`]"
+        :style="{paddingLeft: gutter/2 + 'px', paddingRight: gutter/2 + 'px'}"
+        >
+        <div style="border: 1px solid green; height: 100px;">
+      <slot></slot>
+    </div>
     </div>
 </template>
 
@@ -10,22 +15,35 @@
         props: {
             span: {
                 type:[Number,String]
+            },
+            offset: {
+                type:[Number,String]
+            }
+        },
+        data(){
+            return {
+                gutter: 0
             }
         }
-        
     }
 </script>
 
 <style lang="scss" scoped>
 .col {
-    height: 100px;
-    background: grey;
+    // height: 100px;
+    // background: grey;
     width: 50%;
-    border: 1px solid red;
+    // border: 1px solid red;
     $class-prefix: col-;
     @for $n from 1 through 24 {
         &.#{$class-prefix}#{$n} {
             width: ($n / 24) * 100%;
+        }
+    }
+    $class-prefix: offset-;
+    @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+            margin-left: ($n / 24) * 100%;
         }
     }
 }
--- src/Row.vue
+++ src/Row.vue
@@ -1,12 +1,25 @@
 <template>
-    <div class="row">
+    <div class="row"
+    :style="{marginLeft: -gutter/2 + 'px', marginRight: -gutter/2 + 'px'}"
+    
+    >
         <slot></slot>
     </div>
 </template>
 
 <script>
     export default {
-        name: 'LemonRow'
+        name: 'LemonRow',
+        props: {
+            gutter: {
+                type: [Number, String]
+            }
+        },
+        mounted () {
+            this.$children.forEach((vm) => {
+                vm.gutter = this.gutter
+            })
+        }
     }
 </script>
 
