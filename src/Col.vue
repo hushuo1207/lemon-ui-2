f8de961dd898849cb07ca3f3a01d057ad6539cb2
hushuo <1253740807@qq.com>
2022-01-01 14:31:15 +0800

    默认是iPhone宽度

--- src/Col.vue
+++ src/Col.vue
@@ -24,7 +24,6 @@
             offset: {
                 type:[Number,String]
             },
-            phone: {type: Object, validator,},
             ipad: {type: Object, validator,},
             narrowPc: {type: Object, validator,},
             pc: {type: Object, validator,},
@@ -44,11 +43,10 @@
                 }
             },
             colClass(){
-                let {span, offset, phone, ipad, narrowPc, pc, widePc} = this
+                let {span, offset, ipad, narrowPc, pc, widePc} = this
                 return [
                     span && `col-${span}`, 
                     offset && `offset-${offset}`,
-                    ... (phone && [`col-phone-${phone.span}`]),
                     ... (ipad && [`col-ipad-${ipad.span}`]),
                     ... (narrowPc && [`col-narrow-pc-${narrowPc.span}`]),
                     ... (pc && [`col-pc-${pc.span}`]),
@@ -76,21 +74,21 @@
         &.#{$class-prefix}#{$n} {
             margin-left: ($n / 24) * 100%;
         }
-    }
-    @media (max-width: 576px) {
-      $class-prefix: col-phone-;
-      @for $n from 1 through 24 {
-        &.#{$class-prefix}#{$n} {
-          width: ($n / 24) * 100%;
-        }
-      }
-      $class-prefix: offset-phone-;
-      @for $n from 1 through 24 {
-        &.#{$class-prefix}#{$n} {
-          margin-left: ($n / 24) * 100%;
-        }
-      }
-    }
+     }
+    // @media (max-width: 576px) {
+    //   $class-prefix: col-phone-;
+    //   @for $n from 1 through 24 {
+    //     &.#{$class-prefix}#{$n} {
+    //       width: ($n / 24) * 100%;
+    //     }
+    //   }
+    //   $class-prefix: offset-phone-;
+    //   @for $n from 1 through 24 {
+    //     &.#{$class-prefix}#{$n} {
+    //       margin-left: ($n / 24) * 100%;
+    //     }
+    //   }
+    // }
     @media (min-width: 577px) and (max-width: 768px) {
       $class-prefix: col-ipad-;
       @for $n from 1 through 24 {
