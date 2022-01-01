6f4c8af238b1a0915d344ba5f60539e4a0f4ee4b
hushuo <1253740807@qq.com>
2022-01-01 11:17:19 +0800

    Row and Col雏形完成

--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@ import Button from "./Button.vue";
 import ButtonGroup from "./ButtonGroup.vue";
 import Icon from "./Icon.vue";
 import Input from "./Input.vue"
+import Row from "./Row.vue"
+import Col from "./Col.vue"
 
 Vue.component("g-button", Button);
 Vue.component("g-button-group", ButtonGroup);
 Vue.component("g-icon", Icon);
 Vue.component("g-input", Input);
+Vue.component("g-row", Row);
+Vue.component("g-col", Col);
 
 new Vue({
   el: "#app",
