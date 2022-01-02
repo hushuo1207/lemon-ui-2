1e0ad2c0c0d6829f4c16b0d2556b7877112513dd
hushuo <1253740807@qq.com>
2022-01-02 14:01:46 +0800

    tabs重构

--- src/Tabs.vue
+++ src/Tabs.vue
@@ -1,65 +1,75 @@
 <template>
-    <div class="tabs">
-        <slot></slot>
-
-    </div>
+  <div class="tabs">
+    <slot></slot>
+  </div>
 </template>
 
 <script>
-import Vue from 'vue'
+import Vue from "vue";
 export default {
-    name: "LemonTabs",
-    props: {
-      selected: {
-        type: String,
-        required: true
+  name: "LemonTabs",
+  props: {
+    selected: {
+      type: String,
+      required: true,
+    },
+    direction: {
+      type: String,
+      default: "horizontal",
+      validator(value) {
+        return ["horizontal", "vertical"].indexOf(value) >= 0;
       },
-      direction: {
-        type: String,
-        default: 'horizontal',
-        validator (value) {
-          return ['horizontal', 'vertical'].indexOf(value) >= 0
-        }
-      }
     },
-    data () {
-      return {
-        eventBus: new Vue()
-      }
-    },
-    provide () {
-      return {
-        eventBus: this.eventBus
-      }
-    },
-    mounted () {
+  },
+  data() {
+    return {
+      eventBus: new Vue(),
+    };
+  },
+  provide() {
+    return {
+      eventBus: this.eventBus,
+    };
+  },
+  methods: {
+    checkChildren() {
       if (this.$children.length === 0) {
-        console && console.warn &&
-        console.warn('tabs的子组件应该是tabs-head和tabs-nav，但你没有写子组件')
+        console &&
+          console.warn &&
+          console.warn(
+            "tabs的子组件应该是tabs-head和tabs-nav，但你没有写子组件"
+          );
       }
+    },
+    selectTab() {
       this.$children.forEach((vm) => {
-        if (vm.$options.name === 'LemonTabsHead') {
+        if (vm.$options.name === "LemonTabsHead") {
           vm.$children.forEach((childVm) => {
-            if (childVm.$options.name === 'LemonTabsItem'
-              && childVm.name === this.selected) {
-              this.eventBus.$emit('update:selected', this.selected, childVm)
+            if (
+              childVm.$options.name === "LemonTabsItem" &&
+              childVm.name === this.selected
+            ) {
+              this.eventBus.$emit("update:selected", this.selected, childVm);
             }
-          })
+          });
         }
-      })
-        // this.eventBus.$emit('update:selected',()=>{
-        // this.$emit('update:selected',tabName)
-        // })
-        //this.eventBus.$emit('update:selected', this.selected)
-        // // this.$emit('update:selected', 'xxx')
-    }
+      });
+    },
+  },
+  mounted() {
+    this.checkChildren();
+    this.selectTab();
 
-  
-}
+    // this.eventBus.$emit('update:selected',()=>{
+    // this.$emit('update:selected',tabName)
+    // })
+    //this.eventBus.$emit('update:selected', this.selected)
+    // // this.$emit('update:selected', 'xxx')
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-.tabs{
-
+.tabs {
 }
 </style>
